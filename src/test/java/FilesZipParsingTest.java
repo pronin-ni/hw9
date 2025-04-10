09f6e936e913d254446f16a35205ca2b7f8a569c
npronin <noxywille@yandex.ru>
2025-04-10 18:37:33 +0300

    fix

--- src/test/java/FilesZipParsingTest.java
+++ src/test/java/FilesZipParsingTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.util.Objects;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
@@ -17,65 +18,70 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class FilesZipParsingTest {
     private final ClassLoader classLoader = FilesZipParsingTest.class.getClassLoader();
-    boolean isFileFound = false;
+    String zipFile = "archive_2.zip";
 
     @DisplayName("Проверка парсинга pdf файла в zip архиве")
     @Test
     void pdfFileInZipParsingTest() throws Exception {
-        try (ZipInputStream zip = new ZipInputStream(classLoader.getResourceAsStream("archive.zip"))) {
+        boolean found = false;
+        try (ZipInputStream zip = new ZipInputStream(Objects.requireNonNull(classLoader.getResourceAsStream(zipFile)))) {
             ZipEntry entry;
 
             while ((entry = zip.getNextEntry()) != null) {
                 if (entry.getName().equals("Диплом_Иван_И.1.pdf")) {
-                    isFileFound = true;
                     PDF pdf = new PDF(zip);
+                    found = true;
                     assertTrue(pdf.text.contains("Тестовое мероприятие"));
                 }
             }
-            assertTrue(isFileFound,"Файл не найден");
+            assertTrue(found, "PDF файл не найден в архиве!");
         }
     }
 
     @DisplayName("Проверка парсинга excel файла в zip архиве")
     @Test
     void excelFileInZipParsingTest() throws Exception {
-        try (ZipInputStream zip = new ZipInputStream(classLoader.getResourceAsStream("archive.zip"))) {
+        try (ZipInputStream zip = new ZipInputStream(Objects.requireNonNull(classLoader.getResourceAsStream(zipFile)))) {
             ZipEntry entry;
+            boolean found = false;
 
             while ((entry = zip.getNextEntry()) != null) {
                 if (entry.getName().equals("indicatorsReport (3).xlsx")) {
-                    isFileFound = true;
+                    found = true;
                     XLS xlsx = new XLS(zip);
                     assertTrue(xlsx.excel.getSheetAt(0).getRow(2).getCell(1).getStringCellValue().equals("Перечень показателей деятельности ЦОПП"));
                     assertTrue(xlsx.excel.getSheetAt(0).getRow(3).getCell(1).getStringCellValue().equals("Созданы цифровая платформа и сайт ЦОПП в сети Интернет"));
+                    return;
                 }
             }
-            assertTrue(isFileFound,"Файл не найден");
+            assertTrue(found, "Excel файл не найден в архиве!");
         }
     }
 
     @DisplayName("Проверка парсинга csv файла в zip архиве")
     @Test
     void csvFileInZipParsingTest() throws Exception {
-        try (ZipInputStream zip = new ZipInputStream(classLoader.getResourceAsStream("archive.zip"))) {
+        try (ZipInputStream zip = new ZipInputStream(Objects.requireNonNull(classLoader.getResourceAsStream(zipFile)))) {
             ZipEntry entry;
 
+            boolean found = false;
             while ((entry = zip.getNextEntry()) != null) {
                 if (entry.getName().equals("errors_Spark ru companies adapter_2025-01-17.csv")) {
-                    isFileFound = true;
+                    found = true;
                     CSVParser parser = new CSVParserBuilder()
                             .withSeparator(';')
                             .build();
                     CSVReader csvReader = new CSVReaderBuilder(new InputStreamReader(zip, StandardCharsets.UTF_8))
                             .withCSVParser(parser)
                             .build();
-                        assertThat(csvReader).contains(
-                                new String[] {"770600206610277394386825", "Internal Error", "Request length limit exceeded: 4096"},
-                                new String[] {"77120881501027739729270197", "Decode Error", "Failed to decode result (LoadCompanyLinks): CompanyLinkResponseBody parse error"}
-                        );
-                    }
+                    assertThat(csvReader).contains(
+                            new String[]{"770600206610277394386825", "Internal Error", "Request length limit exceeded: 4096"},
+                            new String[]{"77120881501027739729270197", "Decode Error", "Failed to decode result (LoadCompanyLinks): CompanyLinkResponseBody parse error"}
+                    );
+                    return;
                 }
-            assertTrue(isFileFound,"Файл не найден");
+            }
+            assertTrue(found, "CSV файл не найден в архиве!");
         }
-        }
-    }
+
+    } }
